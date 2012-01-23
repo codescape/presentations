6742ce5535b1420f347d9617ff3333f214474ec8
Stefan Glase <stefan.glase@googlemail.com>
2012-01-23 16:13:11 +0100

    updating sample

--- groovyfx-entfesselt-javafx/samples/src/main/groovy/samples/HelloWorldSample.groovy
+++ groovyfx-entfesselt-javafx/samples/src/main/groovy/samples/HelloWorldSample.groovy
@@ -5,12 +5,12 @@ import groovyx.javafx.GroovyFX
 import javafx.scene.text.Font
 import javafx.scene.text.FontWeight
 
-def myFont = Font.font('Verdana', FontWeight.BOLD, 50)
+def myFont = Font.font('Verdana', FontWeight.BOLD, 64)
 
 GroovyFX.start {
     new SceneGraphBuilder().stage(title: 'Hello World', width: 600, height: 280, visible: true) {
         scene(fill: white) {
-            text(text: 'Hello World', x: 130, y: 130, font: myFont)
+            text(text: 'Hello OOP', x: 120, y: 140, font: myFont, fill: red)
         }
     }
 }
